b8b3099ff61bb012e2a7a56f3e3d548ada2e7281
Ridho Prasetya <ndiing.inc@gmail.com>
2022-08-24 06:56:22 +0700

    Update

--- index.js
+++ index.js
@@ -7,7 +7,72 @@ const Database = require("@ndiinginc/database");
 const { Date2 } = require("@ndiinginc/util");
 
 /**
+ * @example
+ * // ./user.js
+ * // Create router
+ * const user = Router();
+ * // Create middleware
+ * user.use((req, res, next) => {
+ *     next();
+ * });
+ * // Create get middleware
+ * user.get("/", (req, res, next) => {
+ *     res.json({ message: "user get" });
+ * });
+ * // Create patch middleware
+ * user.patch("/:id", (req, res, next) => {
+ *     // res.cookie('name','value')//send cookie
+ *     // res.cookie('name')//remove previous cookie
+ *     res.json({
+ *         // headers:req.headers, // get request headers
+ *         // origin:req.origin, // get origin
+ *         ip:req.ip, // get ip
+ *         // url2:req.url2, // get url object
+ *         path:req.path, // get path
+ *         query:req.query, // get query object
+ *         params:req.params, // get params
+ *         // cookie:req.cookie, // get cookie
+ *     });
+ * });
  *
+ * // ./router.js
+ * // Create router
+ * const router = Router();
+ * // Create middleware
+ * router.use((req, res, next) => {
+ *     next();
+ * });
+ * // Register router
+ * router.use('/user',user);
+ * // Create get middleware
+ * router.get("/", (req, res, next) => {
+ *     res.json({ message: "router get" });
+ * });
+ *
+ * // ./index.js
+ * // Create app
+ * const app = Router({
+ *     // body: null, // default body, set null/false to disable
+ *     security: null, // default security, set null/false to disable
+ *     compression: null, // default compression, set null/false to disable
+ *     cache: null, // default cache, set null/false to disable
+ *     cookie: null, // default cookie, set null/false to disable
+ *     limiter: null, // default limiter, set null/false to disable
+ * });
+ * // Create middleware
+ * app.use((req, res, next) => {
+ *     next();
+ * });
+ * // Register router
+ * app.use('/router',router);
+ * // Create get middleware
+ * app.get("/", (req, res, next) => {
+ *     res.json({ message: "app get" });
+ * });
+ * // Start server
+ * app.listen(3000, () => {
+ *     console.log(3000)
+ * });
  */
 class Router {
     routes = [];
@@ -454,4 +519,21 @@ class Router {
     }
 }
 
-module.exports = Router;
+function Layer(config) {
+    // defaultRoute
+    // defaultError
+    const router = new Router(config);
+    const app = (req, res) => router.handleRequest(req, res);
+    app.routes = router.routes;
+    app.errors = router.errors;
+    const methods = ["use", "connect", "delete", "get", "head", "options", "patch", "post", "put", "trace", "listen"];
+    for (let i = 0; i < methods.length; i++) {
+        const method = methods[i];
+        app[method] = (...args) => router[method](...args);
+    }
+    return app;
+}
+
+Layer.Router = Router;
+
+module.exports = Layer;
