46755ba8334113a068834fbbfaed20fd5422142a
Ridho Prasetya <ndiing.inc@gmail.com>
2024-10-06 10:38:45 +0700

    commit

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const Router=require('./lib/router.js')
+const {compression, messages, cookies, security, cors, authorization, fallback, catchAll}=require('./lib/middleware.js')
+Router.compression=compression
+Router.messages=messages
+Router.cookies=cookies
+Router.security=security
+Router.cors=cors
+Router.authorization=authorization
+Router.fallback=fallback
+Router.catchAll=catchAll
 module.exports=Router
